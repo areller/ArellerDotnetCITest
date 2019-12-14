73840c858f68f9719a732853604e1210a1c3a043
areller <lekararik@gmail.com>
2019-12-13 22:41:08 -0500

    first

---
+++ README.md
@@ -0,0 +1,1 @@
+## This is a test project for testing GitHub Actions for .NET Libraries
---
+++ src/ArellerDotnetCITest.Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArellerDotnetCITest.Library
+{
+    public class Class1
+    {
+        public void Foo()
+        {
+            Console.WriteLine("Foo");
+        }
+    }
+}
